77a8384320bee8547f2211c17c25f26121df85fa
kimyujin <jjks20007@daum.net>
2023-01-16 23:38:34 +0900

    2023.01.16 test

--- animore/src/main/java/com/zerock/controller/mainload.java
+++ animore/src/main/java/com/zerock/controller/mainload.java
@@ -7,11 +7,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import lombok.extern.log4j.Log4j;
+
 @Controller
+@Log4j
 public class mainload {
 	
 	@RequestMapping(value = "/{var}", method = RequestMethod.GET)
 	public String base(@PathVariable String var) {
+		log.info("다시 시도");
 		return var;
 	}
 	
